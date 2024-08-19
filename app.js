fe5e7a2fe151a0d8067ee8732a276c30f9520b13
rajesh4666 <129066507+rajesh4666@users.noreply.github.com>
2024-08-19 18:57:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,24 @@
+let result = document.querySelector("#result");
+
 function Solve(val) {
-    var v = document.getElementById('res');
-    v.value += val;
- }
- function Result() {
-    var num1 = document.getElementById('res').value;
-    try {
-       var num2 = eval(num1.replace('x', '*'));
-       document.getElementById('res').value = num2;
-    } catch {
-       document.getElementById('res').value = 'Error';
-    }
- }
- function Clear() {
-    var inp = document.getElementById('res');
-    inp.value = '';
- }
- function Back() {
-    var ev = document.getElementById('res');
-    ev.value = ev.value.slice(0, -1);
- }
- document.addEventListener('keydown', function (event) {
-    const key = event.key;
-    const validKeys = '0123456789+-*/.%';
-    if (validKeys.includes(key)) {
-       Solve(key === '*' ? 'x' : key);
-    } else if (key === 'Enter') {
-       Result();
-    } else if (key === 'Backspace') {
-       Back();
-    } else if (key.toLowerCase() === 'c') {
-       Clear();
-    }
- });
+   //  var v = document.getElementById('result');
+    result.value += val;
+}
+
+function Result() {
+   // var num1 = document.getElementById('result').value;
+
+      var num2 = eval(result.value.replace('x', '*'));
+      result.value = num2;
+   
+}
+
+function Clear() {
+   //  var inp = document.getElementById('result');
+    result.value = '';
+}
+
+function Back() {
+   //  var ev = document.getElementById('result');
+    result.value = result.value.slice(0, -1);
+}
